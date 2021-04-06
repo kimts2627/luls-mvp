0e0cb2e673f452c501b51d247a50df2bbdb65a09
Seokjae Bae <bsjp9400@likelion.net>
2021-04-06 10:59:05 +0900

    [Backend] <feature>: Google Oauth
    Local에서 테스트를 위한 코드 작성중
    현재 EC2 인스턴스 생성 후 RDS 연결 완료.

--- Server/src/controller/users/login.ts
+++ Server/src/controller/users/login.ts
@@ -1,3 +1,6 @@
+import { json } from 'body-parser';
+import { JsonWebTokenError } from 'jsonwebtoken';
+
 require('dotenv').config();
 
 const clientID = process.env.GOOGLE_CLIENT_ID;
@@ -5,20 +8,41 @@ const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 const axios = require('axios');
 
 export default async (req, res) => {
-  console.log(req.query.code);
-  let google = await axios.post(
-    'https://oauth2.googleapis.com/token',
-    {
-      client_id: clientID,
-      client_secret: clientSecret,
-      code: req.query.code,
-      grant_type: 'authorization_code',
-      redirect_uri: 'http://127.0.0.1:3006/users/googleCallback',
-    },
-    {
-      headers: { Accept: 'application/json' },
-    }
-  );
-  console.log(google.data.access_token);
+  // console.log(req.query.code);
+  let code = req.query.code;
+  let google = await axios
+    .post(
+      'https://accounts.google.com/o/oauth2/token',
+      {
+        client_id: clientID,
+        client_secret: clientSecret,
+        code: code,
+        grant_type: 'authorization_code',
+        redirect_uri: 'http://127.0.0.1:3006/users/login',
+      },
+      {
+        headers: { Accept: 'application/json' },
+      }
+    )
+    .then((google) => {
+      const token = google.data.access_token;
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${token}`,
+          {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+        });
+    });
+
+  console.log(google.data);
   res.send(200, { accessToken: google.data.access_token });
 };
