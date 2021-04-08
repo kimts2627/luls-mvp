710a4d352f260c77e1ef6c290cdbf59fbd214d0a
kimts2628 <tsts_@likelion.net>
2021-04-08 18:19:51 +0900

    [Frontend] <refactor>: refactor next-redux
    refactor next-redux legacy code

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,6 +1,6 @@
 import "../styles/globals.scss";
 import React, { useState, useEffect } from "react";
-import withRedux from "next-redux-wrapper";
+import { createWrapper } from "next-redux-wrapper";
 import { createStore } from "redux";
 import reducer from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
@@ -13,9 +13,12 @@ const MyApp = ({ Component }) => {
   );
 };
 
-const configureStore = (initialState, options) => {
-  const store = createStore(reducer, initialState, composeWithDevTools());
-  return store;
-};
+// const configureStore = (initialState, options) => {
+//   const store = createStore(reducer, initialState, composeWithDevTools());
+//   return store;
+// };
 
-export default withRedux(configureStore)(MyApp);
+const makeStore = (context) => createStore(reducer, composeWithDevTools());
+const wrapper = createWrapper(makeStore, { debug: true });
+
+export default wrapper.withRedux(MyApp);
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -5,11 +5,10 @@ import Main from "../components/home/Main";
 import { useSelector } from "react-redux";
 import SigninModal from "../components/auth/SigninModal";
 import axios from "axios";
+import fetch from "node-fetch";
 
 export default function Home({ data }) {
-  useEffect(() => {
-    console.log(data);
-  }, []);
+  console.log(data);
   const token = useSelector((state) => state.auth.token);
   const isLoginModalOn = useSelector((state) => state.auth.isLoginModalOn);
   return (
@@ -27,9 +26,9 @@ export default function Home({ data }) {
 }
 
 export async function getStaticProps() {
-  const res = await axios.get("https://api.manana.kr/address/korea.json");
-  const data = res.data;
-
+  const res = await fetch("https://api.manana.kr/address/korea.json");
+  const data = await res.json();
+  console.log(data);
   return {
     props: {
       data,
