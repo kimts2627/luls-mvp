165630cccb5f336689c736b0bb0a6d377e58802f
kimts2628 <tsts_@likelion.net>
2021-04-08 15:56:42 +0900

    [Frontend] <test>: Server API test
    server API test

--- client/components/home/Terminal.jsx
+++ client/components/home/Terminal.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Terminal = () => {
+  const [test, handleTest] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://www.likelionustest.com/users/test").then((data) => {
+      handleTest(data.message);
+    });
+  }, []);
+
   return (
     <span className="absolute left-36 top-36 bg-black z-10 shadow-xl rounded-xl w-120 h-72 flex items-center flex-col animate-none">
       <div className="w-full h-6 bg-gray-800 rounded-t-xl flex items-center">
@@ -15,7 +24,7 @@ const Terminal = () => {
       </div>
       <div className="absolute left-3 top-9 flex items-center justify-">
         <h1 className="D2coding text-xl text-white ml-3 overflow-hidden">
-          Hack your life!
+          {test}
         </h1>
         <span className="w-2 h-5 border animate-blink"></span>
       </div>
