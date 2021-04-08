4ae6116611a31435f3176e8987e519beced85ba1
Seokjae Bae <bsjp9400@likelion.net>
2021-04-08 11:51:50 +0900

    Delete redis

--- Server/src/middleware/redisCheck.ts
+++ Server/src/middleware/redisCheck.ts
@@ -1,16 +1,16 @@
-import redis from 'redis';
+// import redis from 'redis';
 
-export const redisCheck: Function = async (user_id: string, Token: any) => {
-  const client = redis.createClient();
-  client.get(user_id, function (err, reply) {
-    if (err) throw err;
-    if (reply) {
-      console.log(reply.toString());
-    } else {
-      client.set(user_id, Token, function (err, result) {
-        if (err) throw err;
-        console.log('caching ' + result);
-      });
-    }
-  });
-};
+// export const redisCheck: Function = async (user_id: string, Token: any) => {
+//   const client = redis.createClient();
+//   client.get(user_id, function (err, reply) {
+//     if (err) throw err;
+//     if (reply) {
+//       console.log(reply.toString());
+//     } else {
+//       client.set(user_id, Token, function (err, result) {
+//         if (err) throw err;
+//         console.log('caching ' + result);
+//       });
+//     }
+//   });
+// };
