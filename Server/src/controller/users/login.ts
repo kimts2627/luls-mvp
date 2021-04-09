5ab0778bcab11a201014b4d9cb2e47e21fe8f7d8
Seokjae Bae <bsjp9400@likelion.net>
2021-04-09 10:41:54 +0900

    Fixed login API

--- Server/src/controller/users/login.ts
+++ Server/src/controller/users/login.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
+import { Request, Response } from 'express';
 import { getGoogleAuthURL } from '../../utils/getGoogleAuthURL';
 
-export default async (req, res) => {
-  return res.redirect(getGoogleAuthURL());
+export default async (req: Request, res: Response) => {
+  return res.send(getGoogleAuthURL());
 };
