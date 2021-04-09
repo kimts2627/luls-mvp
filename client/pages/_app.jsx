8139c30e7ec94c7e369a4be165f862a4de70d4a6
kimts2628 <tsts_@likelion.net>
2021-04-09 18:02:54 +0900

    [Frontend] <test>: Testing Token
    Testing Token
    
    <Footer>

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -5,12 +5,42 @@ import { createStore } from "redux";
 import reducer from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { useRouter } from "next/router";
+import axios from "axios";
+import qs from "qs";
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
 
+  const [AuthCode, handleAuthCode] = useState(null);
+
   useEffect(() => {
-    console.log(router);
+    console.log(router.asPath);
+    const getToken = async () => {
+      if (router.query.code && router.query.scope) {
+        handleAuthCode(`${router.query.code}${router.query.scope}`);
+
+        let query = {
+          code: AuthCode,
+          client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+          client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+          redirect_uri: "https://likelionusa.com",
+          grant_type: "authorization_code",
+        };
+
+        let res = await axios.post(
+          `https://acounts.google.com/o/oauth2/token?${qs.stringify(query)}`,
+          {
+            withCredentials: true,
+            header: { "Content-Type": "application/x-www-form-urlencoded" },
+          }
+        );
+
+        let data = res.data;
+
+        console.log(data);
+      }
+    };
+    getToken();
   });
   return (
     <>
