9289336c00714f99755f95e9a1aea9415181b423
kimts2628 <tsts_@likelion.net>
2021-04-13 16:25:49 +0900

    [Frontend] <style>: Edit Footer structure
    edit footer component structure
    
    <Footer>

--- client/components/layout/Footer.jsx
+++ client/components/layout/Footer.jsx
@@ -2,10 +2,27 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full h-80 flex justify-center bg-white shadow-2xl">
-      <div className="bg-white w-full h-full max-w-screen-2xl border-t border-black  flex items-center justify-center">
-        Footer!
+    <footer className="w-full h-96 flex flex-col items-center justify-center bg-yellow-500 shadow-3xl">
+      <div className="w-full h-full max-w-screen-2xl border flex items-center justify-center">
+        <section className="w-1/2 h-full border-blue-800 border">
+          <h1>ABOUT US</h1>
+        </section>
+        <section className="w-1/2 h-full border-blue-800 border flex">
+          <div className="w-1/2 h-full border-red-900 border">
+            <h1>FIND US</h1>
+          </div>
+          <div className="w-1/2 h-full border-red-900 border">
+            <h1>LINKS</h1>
+          </div>
+        </section>
       </div>
+      <footer className="w-full h-16 bg-yellow-600 flex justify-center">
+        <div className="w-full max-w-screen-2xl">
+          <h1 className="text-white text-md h-full flex justify-center items-center w-64">
+            © 2021 LikeLion inc. /<p className="text-sm">멋쟁이사자처럼</p>
+          </h1>
+        </div>
+      </footer>
     </footer>
   );
 };
