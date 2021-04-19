82d66664aba897aad705ebb30dd43824c4c06381
kimts2628 <tsts_@likelion.net>
2021-04-19 18:15:03 +0900

    [Frontend] <feature>: Add notice modal exit func
    add notice modal exit func

--- client/components/univ/NoticeModal.jsx
+++ client/components/univ/NoticeModal.jsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { handleNoticeModal } from "../../reducers/notice";
 
 const NoticeModal = ({ currentNotice }) => {
+  const dispatch = useDispatch();
   const noticeModal = useSelector((state) => state.notice.noticeModal);
+
+  const handlingNoticeModal = useCallback(() => {
+    dispatch(handleNoticeModal());
+  }, []);
+
   return (
     <aside
       className={`fixed w-full z-20 top-28 ${
         noticeModal ? "left-0" : "left-full"
-      } flex transition-all duration-500`}
+      } flex transition-all duration-300`}
     >
-      <div className="blackback w-1/2 h-screen" />
-      <aside className="w-1/2 h-screen bg-yellow-400">Hello React!</aside>
+      <div
+        className={`w-1/2 h-screen duration-500 ${
+          noticeModal ? "blackback" : null
+        }`}
+        onClick={handlingNoticeModal}
+      />
+      <aside className="w-1/2 h-screen bg-yellow-400">
+        {currentNotice ? currentNotice.content : "no data"}
+      </aside>
     </aside>
   );
 };
