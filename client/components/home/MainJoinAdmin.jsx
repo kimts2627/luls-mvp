f4b070e781c83f9f7baedbf056f94f86eec6c1f9
kimts2628 <tsts_@likelion.net>
2021-04-21 18:26:39 +0900

    [Frontend] <style>: Edit join us style
    edit join us style

--- client/components/home/MainJoinAdmin.jsx
+++ client/components/home/MainJoinAdmin.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef } from "react";
 const MainJoinAdmin = () => {
   const textRef = useRef();
   const divRef = useRef();
+  const descRef = useRef();
 
   const adminEvent = () => {
     if (divRef !== null) {
@@ -12,6 +13,10 @@ const MainJoinAdmin = () => {
       ) {
         textRef.current.style.opacity = "1";
         textRef.current.style.bottom = "13rem";
+        setTimeout(() => {
+          descRef.current.style.opacity = "1";
+          descRef.current.style.bottom = "4rem";
+        }, 500);
       }
     }
   };
@@ -39,20 +44,54 @@ const MainJoinAdmin = () => {
     };
   }, []);
 
+  //*######################################################
+
+  const orRegRef = useRef();
+  const bkRegRef = useRef();
+  let orDeg = -12;
+  let bkDeg = 6;
+
+  const rotateProps = (e) => {
+    if (e.wheelDelta >= 0) {
+      orDeg = orDeg - 1;
+      bkDeg = bkDeg + 1;
+    } else {
+      orDeg = orDeg + 1;
+      bkDeg = bkDeg - 1;
+    }
+    orRegRef.current.style.transform = `rotate(${orDeg}deg)`;
+    bkRegRef.current.style.transform = `rotate(${bkDeg}deg)`;
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousewheel", throttle(rotateProps, 100), {
+      passive: true,
+    });
+    return () => {
+      window.removeEventListener("mousewheel", throttle(rotateProps, 100), {
+        passive: true,
+      });
+    };
+  }, []);
+
+  //*######################################################
+
   return (
     <div
-      className="relative w-full max-w-screen-2xl h-150 text-9xl flex items-center justify-center"
+      className="relative w-full max-w-screen-2xl h-150 flex items-center justify-center"
       ref={divRef}
     >
       <img
         src="/img/reg-or.png"
         alt=""
-        className="absolute -top-4 right-0 transform -rotate-12 z-20 shadow-md"
+        className="absolute -top-4 right-0 transform -rotate-12 z-20 shadow-md transition-all duration-75"
+        ref={orRegRef}
       />
       <img
         src="/img/reg-bk.png"
         alt=""
-        className="w-120 absolute top-4 right-24 transform rotate-6 z-10 shadow-md"
+        className="w-120 absolute top-4 right-24 transform rotate-6 z-10 shadow-md transition-all duration-75"
+        ref={bkRegRef}
       />
       <div className="w-full absolute top-0 left-0 h-120 overflow-hidden shadow-lg">
         <img src="/img/joinadmin.jpg" alt="" className="w-full" />
@@ -65,6 +104,14 @@ const MainJoinAdmin = () => {
           JOIN <span className="text-yellow-500">LIKELION</span> as Admin
         </h1>
       </div>
+      <p
+        className="absolute w-120 right-20 -bottom-24 opacity-0 text-center text-xl transition-all duration-1000"
+        ref={descRef}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam justo odio,
+        egestas nec massa sit amet, tincidunt tempor massa. Proin rutrum congue
+        nulla non pulvinar. Aliquam tempor varius laoreet.
+      </p>
     </div>
   );
 };
--- client/components/home/MainJoinStudent.jsx
+++ client/components/home/MainJoinStudent.jsx
@@ -3,6 +3,7 @@ import React, { useRef, useEffect } from "react";
 const MainJoinStudent = () => {
   const textRef = useRef();
   const divRef = useRef();
+  const descRef = useRef();
 
   const studentEvent = () => {
     if (divRef !== null) {
@@ -12,6 +13,10 @@ const MainJoinStudent = () => {
       ) {
         textRef.current.style.opacity = "1";
         textRef.current.style.bottom = "13rem";
+        setTimeout(() => {
+          descRef.current.style.opacity = "1";
+          descRef.current.style.bottom = "4rem";
+        }, 500);
       }
     }
   };
@@ -39,6 +44,33 @@ const MainJoinStudent = () => {
     };
   }, []);
 
+  //*######################################################
+
+  const bkRegRef = useRef();
+  let bkDeg = 6;
+
+  const rotateProps = (e) => {
+    if (e.wheelDelta >= 0) {
+      bkDeg = bkDeg + 1;
+    } else {
+      bkDeg = bkDeg - 1;
+    }
+    bkRegRef.current.style.transform = `rotate(${bkDeg}deg)`;
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousewheel", throttle(rotateProps, 100), {
+      passive: true,
+    });
+    return () => {
+      window.removeEventListener("mousewheel", throttle(rotateProps, 100), {
+        passive: true,
+      });
+    };
+  }, []);
+
+  //*######################################################
+
   return (
     <div
       className="relative w-full max-w-screen-2xl h-150 mt-60 mb-44 flex items-center justify-center"
@@ -47,7 +79,8 @@ const MainJoinStudent = () => {
       <img
         src="/img/triangle-bk.png"
         alt=""
-        className="absolute -top-40 left-56 w-80 transform -rotate-12 z-20"
+        className="absolute -top-40 left-56 w-80 transform -rotate-12 z-20 transition-all duration-75"
+        ref={bkRegRef}
       />
       <img
         src="/img/donut-or.png"
@@ -65,6 +98,14 @@ const MainJoinStudent = () => {
           JOIN <span className="text-yellow-500">LIKELION</span> as Member
         </h1>
       </div>
+      <p
+        className="absolute w-120 left-20 -bottom-24 opacity-0 text-center text-xl transition-all duration-1000"
+        ref={descRef}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam justo odio,
+        egestas nec massa sit amet, tincidunt tempor massa. Proin rutrum congue
+        nulla non pulvinar. Aliquam tempor varius laoreet.
+      </p>
     </div>
   );
 };
