c76ee13a080da5361b9e774a5cbe0db666feee54
kimts2628 <tsts_@likelion.net>
2021-04-27 18:08:33 +0900

    [Frontend] <fix>: Fix something
    fix error

--- client/pages/task/post/[id].jsx
+++ client/pages/task/post/[id].jsx
@@ -8,11 +8,23 @@ const Posts = () => {
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
+    // ! 개별 페이지 정보 받아와야 함
     // axios.get('https://likelionustest.com/bulletin/', {
     // })
     // .then(res => res.data)
     // .then()
   }, []);
+
+  const returnButtonColor = () => {
+    switch (currentTaskPost.submit_check) {
+      case "1":
+        return "bg-red-100";
+      case "2":
+        return "bg-green-100";
+      default:
+        return "bg-white";
+    }
+  };
 
   return (
     <Layout>
@@ -21,10 +33,19 @@ const Posts = () => {
           <div className="animate-rotate">LOADING</div>
         ) : (
           <div className="w-1/2 h-screen text-4xl flex flex-col justify-center items-center">
-            <div className="bg-red-50 w-full h-1/2 flex flex-col justify-evenly items-center">
-              <h1>{`Content Id Num is ${currentTaskPost.id} and Author is ${currentTaskPost.name}`}</h1>
-              <h2>{currentTaskPost.title}</h2>
+            <div className="relative bg-red-50 w-full h-1/2 flex flex-col justify-evenly items-center">
+              <h1 className="absolute left-5 top-5">{currentTaskPost.id}</h1>
+              <h1 className="absolute right-5 top-5">
+                {currentTaskPost.school}
+              </h1>
+              <h2 className="text-2xl">{currentTaskPost.title}</h2>
               <h3>{currentTaskPost.content}</h3>
+              <div>
+                <button className={`rounded-lg ${returnButtonColor}`}>
+                  Pass
+                </button>
+                <button>noPass</button>
+              </div>
             </div>
           </div>
         )}
