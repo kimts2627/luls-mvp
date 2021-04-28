0ec0dbda31ee9961a8ad0723ae8ad90bbcfe7b7d
kimts2628 <tsts_@likelion.net>
2021-04-28 10:57:00 +0900

    [Frontend] <feature>: Add task status modify function
    add task status modify function called modifyTaskStatus

--- client/pages/task/post/[id].jsx
+++ client/pages/task/post/[id].jsx
@@ -1,50 +1,98 @@
 import axios from "axios";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Layout from "../../../components/layout";
 
 const Posts = () => {
-  const currentTaskPost = useSelector((state) => state.task.currentTaskPost);
+  const [currentTaskPost, setPost] = useState({});
+  const router = useRouter();
   const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
+  const getCurrentTask = () => {
     // ! 개별 페이지 정보 받아와야 함
-    // axios.get('https://likelionustest.com/bulletin/', {
-    // })
-    // .then(res => res.data)
-    // .then()
+    let id = router.asPath.slice(router.asPath.length - 2);
+    let token = window.localStorage.getItem("ac-token");
+    console.log(id);
+    axios
+      .get(`https://likelionustest.com/bulletin/list/${id}`, {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => res.data)
+      .then((data) => {
+        console.log(data);
+        setPost(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  useEffect(() => {
+    getCurrentTask();
   }, []);
 
-  const returnButtonColor = () => {
-    switch (currentTaskPost.submit_check) {
-      case "1":
-        return "bg-red-100";
-      case "2":
-        return "bg-green-100";
-      default:
-        return "bg-white";
-    }
+  const modifyTaskStatus = (status) => {
+    let id = router.asPath.slice(router.asPath.length - 2);
+    let token = window.localStorage.getItem("ac-token");
+
+    axios
+      .post(
+        `https://likelionustest.com/bulletin/submit`,
+        {
+          id: id,
+          submit_check: status,
+        },
+        { withCredentials: true, headers: { Authorization: `Bearer ${token}` } }
+      )
+      .then((res) => res.data)
+      .then((data) => {
+        console.log(data);
+        getCurrentTask();
+      });
   };
 
   return (
     <Layout>
       <div className="w-full h-full mt-28 flex items-center justify-center">
         {!currentTaskPost ? (
-          <div className="animate-rotate">LOADING</div>
+          <div className="w-full h-full flex items-center justify-center">
+            LOADING
+          </div>
         ) : (
           <div className="w-1/2 h-screen text-4xl flex flex-col justify-center items-center">
-            <div className="relative bg-red-50 w-full h-1/2 flex flex-col justify-evenly items-center">
+            <div className="relative bg-white shadow-xl w-full h-1/2 flex flex-col justify-evenly items-center">
               <h1 className="absolute left-5 top-5">{currentTaskPost.id}</h1>
               <h1 className="absolute right-5 top-5">
                 {currentTaskPost.school}
               </h1>
               <h2 className="text-2xl">{currentTaskPost.title}</h2>
               <h3>{currentTaskPost.content}</h3>
-              <div>
-                <button className={`rounded-lg ${returnButtonColor}`}>
+              <div className="absolute bottom-10 flex justify-between w-60">
+                <button
+                  className={`rounded-lg border-4 ${
+                    currentTaskPost.submit_check === "pass"
+                      ? "border-green-500"
+                      : ""
+                  } p-2 bg-gray-100`}
+                  onClick={() => modifyTaskStatus("pass")}
+                >
                   Pass
                 </button>
-                <button>noPass</button>
+                <button
+                  className={`rounded-lg border-4 ${
+                    currentTaskPost.submit_check === "fail"
+                      ? "border-red-500"
+                      : ""
+                  } p-2 bg-gray-100`}
+                  onClick={() => modifyTaskStatus("fail")}
+                >
+                  Fail
+                </button>
               </div>
             </div>
           </div>
