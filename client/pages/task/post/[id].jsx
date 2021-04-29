f376e8974b5a1d379b098db82b94be20aa0e010a
kimts2628 <tsts_@likelion.net>
2021-04-29 13:16:17 +0900

    [Frontend] <fix>: Fix something
    fix something

--- client/pages/task/post/[id].jsx
+++ client/pages/task/post/[id].jsx
@@ -5,32 +5,32 @@ import { useDispatch, useSelector } from "react-redux";
 import Layout from "../../../components/layout";
 import { handleModifyTaskInfo } from "../../../reducers/task";
 
-// export async function getStaticPaths() {
-//   const response = await axios(
-//     "https://www.likelionustest.com/bulletin/hwlist?school=멋사대학교"
-//   );
-//   const postList = await response.data.bulletin;
-//   console.log(postList);
-//   return {
-//     paths: postList.map((post) => {
-//       return {
-//         params: {
-//           id: `${post.id}`,
-//         },
-//       };
-//     }),
-//     fallback: false,
-//   };
-// }
+export async function getStaticPaths() {
+  const response = await axios(
+    "https://www.likelionustest.com/bulletin/hwlist?school=멋사대학교"
+  );
+  const postList = await response.data.bulletin;
+  console.log(postList);
+  return {
+    paths: postList.map((post) => {
+      return {
+        params: {
+          id: `${post.id}`,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
 
-// export async function getStaticProps({ params }) {
-//   const post = { no: "no" };
-//   return {
-//     props: post,
-//   };
-// }
+export async function getStaticProps({ params }) {
+  const post = { no: "no" };
+  return {
+    props: post,
+  };
+}
 
-const Posts = () => {
+const Posts = (props) => {
   const dispatch = useDispatch();
   const [currentTaskPost, setPost] = useState({});
   const router = useRouter();
@@ -66,6 +66,7 @@ const Posts = () => {
   };
 
   useEffect(() => {
+    console.log(props);
     getCurrentTask();
   }, []);
 
