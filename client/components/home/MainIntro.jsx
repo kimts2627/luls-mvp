060bd3f68a9a69c3ed297e61ac8c8503d777eb45
kimts2628 <tsts_@likelion.net>
2021-04-30 12:28:58 +0900

    [Frontend] <refactor>: delete terminal
    delete terminal component

--- client/components/home/MainIntro.jsx
+++ client/components/home/MainIntro.jsx
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useRef, useState } from "react";
-import Terminal from "./Terminal";
 
 const NewsBar = () => {
   const [currentNews, setNews] = useState(0);
