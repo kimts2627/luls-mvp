d52915b826edf9c948095008b03f0f25de038106
kimts2628 <tsts_@likelion.net>
2021-05-03 13:48:01 +0900

    [Frontend] <fix>: Fix routes error
    fix router syntax error

--- client/pages/tasks/posts/index.jsx
+++ client/pages/tasks/posts/index.jsx
@@ -22,7 +22,7 @@ const SingleTask = ({ post }) => {
   };
 
   return (
-    <Link href={`/task/post/${post.id}`} as={`/task/post/${post.id}`}>
+    <Link href={`/tasks/posts/${post.id}`} as={`/tasks/posts/${post.id}`}>
       <div
         className={`w-full h-16 shadow-md border mb-2 cursor-pointer flex items-center justify-evenly ${returnButtonColor()}`}
       >
