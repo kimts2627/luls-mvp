4f4d63de42d60dc0107571da355e3f42ae052196
kimts2628 <tsts_@likelion.net>
2021-05-04 17:41:55 +0900

    [Frontend] <feature>: attendance comment feature
    only non-check or delay can post comments

--- client/pages/admins/attendances.jsx
+++ client/pages/admins/attendances.jsx
@@ -3,20 +3,39 @@ import React, { useEffect, useState, useRef } from "react";
 import Layout from "../../components/layout";
 import StatusModal from "../../components/attendance/StatusModal";
 
-const AttendanceChart = ({ attendance }) => {
+const AttendanceChart = ({
+  attendance,
+  setTrigger,
+  dataChangeTrigger,
+  currentComment,
+  setComment,
+}) => {
   const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const statusRef = useRef();
 
-  const attendanceStatusColor = (status) => {
-    switch (status) {
+  const setAttendanceStatusColor = (week) => {
+    switch (week.status) {
       case 1:
-        return "bg-green-300";
+        return "bg-green-300 text-green-700";
       case 2:
-        return "bg-yellow-300";
+        return "bg-yellow-300 text-yellow-700";
       case 3:
-        return "bg-red-300";
+        return "bg-red-300 text-red-800";
       default:
         return "bg-gray-50";
+    }
+  };
+
+  const setAttendanceStatusComment = (week) => {
+    switch (week.status) {
+      case 1:
+        return "Checked";
+      case 2:
+        return !week.comment ? "" : week.comment;
+      case 3:
+        return !week.comment ? "Non-checked" : week.comment;
+      default:
+        return "";
     }
   };
 
@@ -47,11 +66,19 @@ const AttendanceChart = ({ attendance }) => {
               {student.mem_att.map((week) => (
                 <div
                   key={week.att.date}
-                  className={`border w-32 h-full ${attendanceStatusColor(
-                    week.status
-                  )} cursor-pointer relative box flex justify-center items-center status`}
+                  className={`border w-32 h-full ${setAttendanceStatusColor(
+                    week
+                  )} cursor-pointer relative box flex justify-center items-center status text-xs`}
                 >
-                  <StatusModal student={student} week={week} />
+                  {setAttendanceStatusComment(week)}
+                  <StatusModal
+                    student={student}
+                    week={week}
+                    setTrigger={setTrigger}
+                    dataChangeTrigger={dataChangeTrigger}
+                    currentComment={currentComment}
+                    setComment={setComment}
+                  />
                 </div>
               ))}
             </div>
@@ -64,6 +91,11 @@ const AttendanceChart = ({ attendance }) => {
 
 const Attendances = () => {
   const [attendance, setAttendance] = useState([]);
+  const [dataChangeTrigger, setTrigger] = useState(false);
+  const [currentComment, setComment] = useState("");
+  const [selectedComment, selectComment] = useState("");
+
+  const inputRef = useRef();
 
   let boxes;
 
@@ -87,26 +119,55 @@ const Attendances = () => {
         });
       }
     };
-  }, [boxes]);
+  }, [dataChangeTrigger]);
 
   const handleHoverEvent = () => {
     boxes = document.querySelectorAll(".box");
     for (let i of boxes) {
       i.addEventListener("mouseenter", (e) => {
         i.children[0].classList.remove("hidden");
+        selectComment(i.textContent.split("checknon")[0]);
       });
       i.addEventListener("mouseleave", (e) => {
         i.children[0].classList.add("hidden");
+        selectComment("");
       });
     }
   };
+
+  const handleChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  useEffect(() => {
+    inputRef.current.value = currentComment;
+  }, [currentComment]);
 
   return (
     <Layout>
       <div className="w-full h-smallFull flex justify-center">
         <section className="w-full max-w-screen-xl h-full bg-blue-100 flex flex-col items-center justify-center">
           <h1 className="text-5xl mb-10">Attendance</h1>
-          <AttendanceChart attendance={attendance} />
+          <AttendanceChart
+            attendance={attendance}
+            setTrigger={setTrigger}
+            dataChangeTrigger={dataChangeTrigger}
+            currentComment={currentComment}
+            setComment={setComment}
+          />
+          <p className="text-gray-800 h-10 mt-10">{selectedComment}</p>
+          <section className="mt-6 w-120 h-8 flex bg-white shadow-md z-10">
+            <input
+              type="text"
+              className="flex-1 h-full p-2"
+              placeholder="post comments here and select status button"
+              onChange={(e) => handleChange(e)}
+              ref={inputRef}
+            />
+          </section>
+          <p className="text-red-600">
+            Only non-check or delay can post comments
+          </p>
         </section>
       </div>
     </Layout>
