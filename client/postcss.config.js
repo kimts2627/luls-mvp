608e1f1c32824934b3b612ebdf31a76c5dac7c6a
kimts2628 <tsts_@likelion.net>
2021-05-14 18:34:28 +0900

    postcss test

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,23 +1,21 @@
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// };
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    autoprefixer: {},
-    tailwindcss: {},
-    "postcss-flexbugs-fixes": {},
-    "postcss-preset-env": {
-      autoprefixer: {
-        flexbox: "no-2009",
-      },
-      stage: 3,
-      features: {
-        "custom-properties": false,
-      },
-    },
-  },
+  plugins: [
+      'postcss-import',
+      'tailwindcss',
+      'postcss-nesting',
+      'postcss-flexbugs-fixes',
+      [
+          'postcss-preset-env',
+          {
+              autoprefixer: {
+                  flexbox: 'no-2009'
+              },
+              stage: 3,
+              features: {
+                  'custom-properties': false,
+                  'nesting-rules': true
+              }
+          }
+      ]
+  ]
 };
