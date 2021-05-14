1cc3020b53b0a2c4ada68ffbff130767d3e8d25e
kimts2628 <tsts_@likelion.net>
2021-05-14 18:43:57 +0900

    postcss test

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,21 +1,46 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
 module.exports = {
-  plugins: [
-      'postcss-import',
-      'tailwindcss',
-      'postcss-nesting',
-      'postcss-flexbugs-fixes',
-      [
-          'postcss-preset-env',
-          {
-              autoprefixer: {
-                  flexbox: 'no-2009'
-              },
-              stage: 3,
-              features: {
-                  'custom-properties': false,
-                  'nesting-rules': true
-              }
-          }
-      ]
-  ]
+  plugins: {
+    "postcss-import": {},
+    autoprefixer: {},
+    tailwindcss: {},
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 3,
+      features: {
+        "custom-properties": false,
+        'nesting-rules': true
+      },
+    },
+  },
 };
+
+// module.exports = {
+//   plugins: [
+//     'postcss-import',
+//     'tailwindcss',
+//     'postcss-nesting',
+//     'postcss-flexbugs-fixes',
+//     [
+//       'postcss-preset-env',
+//       {
+//         autoprefixer: {
+//           flexbox: 'no-2009'
+//         },
+//         stage: 3,
+//         features: {
+//           'custom-properties': false,
+
+//         }
+//       }
+//     ]
+//   ]
+// };
